170c405f23cb9c615d05edc8f371325391b23f46
Titus <handsomekhalo@gmail.com>
2024-12-19 11:11:29 +0200

    adding user model

---
+++ system_management/constants.py
@@ -0,0 +1,13 @@
+# User Types
+ADMIN = "ADMIN"
+RESTAURENT_ADMIN ="RESTAURENT_ADMIN"
+CUSTOMER = "CUSTOMER"
+DRIVER = "DRIVER"
+
+# application types
+EMPTY = ''
+# API components
+JSON_APPLICATION = 'application/json'
+INVALID_REQUEST_METHOD = "Invalid request: method not allowed."
+
+#case form  types constants"""
